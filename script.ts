80d2a7a1a1f987abfb9c3ae2e7ed459de24822fc
Alex Bogut <alexbogut11@gmail.com>
2023-01-10 13:55:26 -0500

    filled database

--- script.ts
+++ script.ts
@@ -1,12 +1,21 @@
 import { PrismaClient } from "@prisma/client";
+// import { faker } from "@faker-js/faker";
 const prisma = new PrismaClient();
 
 async function main() {
-  await prisma.user.deleteMany();
-  const user = await prisma.user.create({
+  const user = await prisma.comment.create({
     data: {
-      name: "test",
-      email: "test@prisma.io",
+      user: {
+        connect: {
+          id: "9067e6c1-3cad-4b91-8abb-bf8ed018e329",
+        },
+      },
+      text: "Very Cool !",
+      Card: {
+        connect: {
+          id: "689bc2b3-47d7-4f1c-956f-53f85dc4e034",
+        },
+      },
     },
   });
   console.log(user);
--- server/routes/main.js
+++ server/routes/main.js
@@ -1,5 +1,4 @@
 
-
 const router = require("express").Router();
 const generateFakeData = require('')//import funciton from somewhere
 
