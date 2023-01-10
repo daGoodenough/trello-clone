3f630152a5c53054864bee1b8185fe05c657d422
Jack Mullinkosson <jackmullinkosson@Jacks-MacBook-Pro.local>
2023-01-10 10:56:07 -0600

    workflow lists sketches

--- src/components/BoardDetail/WorkflowList.js
+++ src/components/BoardDetail/WorkflowList.js
@@ -1,9 +1,18 @@
 import Card from './Card';
 
-function WorkflowList() {
+function WorkflowList({title}) {
+
+  const cards = ['butter', 'milk', 'eggs']
+  
     return (
-      <div>
-       <Card />
+      <div className="workflow-item">
+        <div className='workflow-wrapper'>
+        <h5>{title}</h5>
+        <ul className="list-ul">
+        {cards.map((i => <Card item={i}/>))}
+        </ul>
+        </div>
+        <div className='add-card-btn'>+ Add a card</div>
       </div>
     );
   }
