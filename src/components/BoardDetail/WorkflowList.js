807dc15a9cadbfbd8919501c4062f9baee38d05d
Jack Mullinkosson <jackmullinkosson@Jacks-MacBook-Pro.local>
2023-01-10 16:39:16 -0600

    can add list items again, still have bug with drag n drop likely caused by the cards state not being part of global store

--- src/components/BoardDetail/WorkflowList.js
+++ src/components/BoardDetail/WorkflowList.js
@@ -5,28 +5,38 @@ import {useDrop} from 'react-dnd'
 
 function WorkflowList({title}) {
   const [workflow, setWorkflow] = useState('To Do')
+  const [currentValue, setCurrentValue] = useState('')
   const [cards, setCards] = useState([{id: 1, title: 'butter', workflow: workflow},{id: 2, title: 'milk', workflow: workflow},{id: 3, title: 'eggs', workflow: workflow}])
   const [newCard, setNewCard] = useState({id: null, title: ''})
-  const [isEditingCard, setIsEditingCard] = useState(false)
+  const [isComposingCard, setIsComposingCard] = useState(false)
+  
 
   useEffect(()=>{
     setCards([...cards, newCard])
+    setCurrentValue('')
+    setIsComposingCard(false)
   },[newCard])
 
-  const handleAddCard =() =>{
-    setIsEditingCard(false)
-  }
 
   const [{ isOver }, drop] = useDrop({
     accept: 'card',
     drop: (card) => {
       console.log(card.title, ' dropped!')
-      setNewCard({id: 6, title: card.title, workflow: title})
+      const selected = card.title
+      console.log(selected)
+      const nextCards = cards.map((i)=>{
+        if(i.title===selected) return{
+          ...i,
+          workflow: title
+        }
+        else return i
+      })
+      setCards(nextCards)
     },
   })
   
     return (
-      <div ref={drop} className="workflow-item" id={title}>
+      <div ref={drop} className="workflow-item">
         <div className='workflow-wrapper'>
         <h5>{title}</h5>
         <ul className="list-ul">
@@ -37,11 +47,11 @@ function WorkflowList({title}) {
         </ul>
         </div>
         <div className="add-card-section" >
-          <div className='add-card-btn' style={{display: isEditingCard ? 'none' : 'block'}} onClick={() => setIsEditingCard(true)}>+ Add a card</div>
-          <div className='card-composer' style={{display: isEditingCard ? 'block' : 'none'}}>
-          <input type="text" placeholder='Enter a title for this card' value={newCard} onChange={(e)=>setNewCard({id: cards.length+1, title: e.target.value})}></input>
-          <button className='btn btn-primary'>Add card</button>
-         <Backspace className="close-composer" onClick={() => setIsEditingCard(false)}/>
+          <div className='add-card-btn' style={{display: isComposingCard ? 'none' : 'block'}} onClick={() => setIsComposingCard(true)}>+ Add a card</div>
+          <div className='card-composer' style={{display: isComposingCard ? 'block' : 'none'}}>
+          <input type="text" placeholder='Enter a title for this card' value={currentValue} onChange={(e)=> setCurrentValue(e.target.value)}></input>
+          <button className='btn btn-primary' onClick={()=> setNewCard({id: cards.length+1, title: currentValue, workflow: title})}>Add card</button>
+         <Backspace className="close-composer" onClick={() => setIsComposingCard(false)}/>
             </div>
           </div>
       </div>
