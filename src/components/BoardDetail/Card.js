ee535445f6d2a7ae1cf7d7c75be98ee0921aad59
Jack Mullinkosson <jackmullinkosson@Jacks-MacBook-Pro.local>
2023-01-10 15:11:14 -0600

    dnd working, broke other stuff though

--- src/components/BoardDetail/Card.js
+++ src/components/BoardDetail/Card.js
@@ -1,16 +1,16 @@
 import { useDrag } from 'react-dnd'
-import { ItemTypes } from './Constants'
 import { Pencil, Backspace } from 'react-bootstrap-icons'
 import { useState } from 'react'
 
-const Card = ({title}) => {
+const Card = ({title, id}) => {
 
   const [isHovering, setIsHovering] = useState(false)
+  const [cardTitle, setCardTitle] = useState(title)
   const [isEditingTitle, setIsEditingTitle] = useState(false)
   const [{ opacity }, dragRef] = useDrag(
     () => ({
-      type: ItemTypes.CARD,
-      item: { title },
+      type: "card",
+      item:  {title, id} ,
       collect: (monitor) => ({
         opacity: monitor.isDragging() ? 0.5 : 1
       })
@@ -21,14 +21,14 @@ const Card = ({title}) => {
   return (
       <div className="card-item">
         <div  ref={dragRef} style={{ opacity, display: isEditingTitle ? 'none' : 'block', }} onMouseEnter={()=>setIsHovering(true)} onMouseLeave={()=>setIsHovering(false)}>
-          {title}
+          {cardTitle}
           <div className='card-pencil'>
             <Pencil style={{display: isHovering ? 'inline-flex' : 'none',}} onClick={() => setIsEditingTitle(true)}/>
             </div>
             </div>
             <div className='card-title-editor' style={{display: isEditingTitle ? 'flex' : 'none',}}>
-              <input type="text" value={title}></input>
-              <button className='btn btn-primary'>Save</button>
+              <input type="text" value={cardTitle} onChange={(e) => setCardTitle(e.target.value)}></input>
+              <button className='btn btn-primary' onClick={() => setIsEditingTitle(false)}>Save</button>
               <Backspace className="close-title-editor" onClick={() => setIsEditingTitle(false)}/>
             </div>
             </div>
