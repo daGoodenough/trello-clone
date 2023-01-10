e655478754a4162a0c41644af178e9ea7faa0662
Damian Goodenough <dgoodenough@proton.me>
2023-01-10 13:41:16 -0800

    set passport up to use sessions

--- server/server.js
+++ server/server.js
@@ -1,18 +1,37 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const passport = require('passport');
+const session = require('express-session');
+
 
 const app = express();
 
 app.use(cors());
 
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+app.use(session({
+  secret: 'your-secret-key',
+  resave: false,
+  saveUninitialized: true,
+  cookie: {}
+}));
+
+passport.serializeUser((user, done) => {
+  console.log("User before serializing: ", user)
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  // this is where you could find the user in the database from the id
+  // done(null, user);
+})
 
 
 const routes = require('./routes/main');
 app.use(routes);
-//imports all routes to be used in the app
 
 
 const port = process.env.PORT || 5000;
