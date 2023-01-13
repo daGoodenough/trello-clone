6dde71f6af399d3c37c93d815f9d5215fdd2e4e3
Damian Goodenough <dgoodenough@proton.me>
2023-01-13 07:44:44 -0800

    find user by password on login

--- server/services/passport.js
+++ server/services/passport.js
@@ -1,24 +1,25 @@
 require('dotenv').config();
+const PrismaClient = require("@prisma/client").PrismaClient;
+const prisma = new PrismaClient();
 const passport = require('passport');
 const LocalStrategy = require('passport-local');
-const {validPassword} = require('./passwords')
+const { validPassword } = require('./passwords')
 
 const localOptions = { usernameField: 'email' };
 
-const localLogin = new LocalStrategy(localOptions, function(email, password, done) {
-  console.log(email, password);
+const localLogin = new LocalStrategy(localOptions, async function (email, password, done) {
   // Verify this email and password, call done with the user
   // if it is the correct email and password
   // otherwise, call done with false
-  console.log("Valid?: ", validPassword(password))
-  // FIND USER BY EMAIL IN DB
-    //if not found return done(null, false)
-    //if found check password
-      if(!validPassword(password)){
-        return done(null, false, { message: 'Incorrect password.' })
-      }
-    //return done(null, user);
-    return done(null, email);
+  const user = await prisma.user.findFirst({ where: { email: email } })
+  if (!user) {
+    return done(null, false)
+  }
+  console.log("User", user)
+  if (!validPassword(password)) {
+    return done(null, false, { message: 'Incorrect password.' })
+  }
+  return done(null, user);
 });
 
 passport.use(localLogin)
