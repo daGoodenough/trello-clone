0a45de1bb4ff039a52de127cecd8cb3fe452c049
Damian Goodenough <dgoodenough@proton.me>
2023-01-13 09:53:22 -0800

    navigate to login if not lgged in

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
+import { useSelector } from 'react-redux';
+import { useNavigate } from "react-router-dom";
+import { useEffect } from 'react';
 import Header from './components/Header';
 import Main from './components/Main';
+import Login from './components/Login';
 
 function App() {
+  const navigate = useNavigate();
+  const loggedIn = useSelector(state => state.loggedIn)
+
+  useEffect(() => {
+    if (!loggedIn) {
+      navigate("/login");
+    }
+  }, [loggedIn]);
+
   return (
     <div className="App">
       <Header />
