c0cb7dea2d3f6a1c239aabbf0c69b64bb2bc571c
Damian Goodenough <dgoodenough@proton.me>
2023-01-13 09:05:28 -0800

    send auth bearer token with requests

--- src/helpers/postData.js
+++ src/helpers/postData.js
@@ -1,5 +1,6 @@
 import axios from 'axios'
 
+axios.defaults.headers.common['Authorization'] = "Bearer " + localStorage.getItem("token");
 
 export const postComment = async (cardId, commentToPost) => {
     try{
