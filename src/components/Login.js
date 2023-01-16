b0c0f73f5deab547edfbe71d948aae82e9fbe394
Damian Goodenough <dgoodenough@proton.me>
2023-01-15 17:10:14 -0800

    show error if 401 or 404 from server

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,32 +1,26 @@
-import {localLogin} from '../actions/';
+import { localLogin } from '../actions/';
 import { Card, Form, Container, Row, Col, Button, Nav } from 'react-bootstrap';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
+import { InfoCircle } from 'react-bootstrap-icons';
 
 function Login() {
+  const { errorMessage } = useSelector(state => state.auth)
   const dispatch = useDispatch();
   const navigate = useNavigate();
-  
+
   const handleLocalLogin = (e) => {
     dispatch(localLogin(e, () => {
       navigate('/')
     }))
   }
-  
+
   return (
     <Container className='mt-5'>
       <Row className="align-items-center justify-content-center">
         <Col md={5}>
           <Card border="secondary" className="shadow-lg p-3 mb-5 bg-body-tertiary rounded ">
             <Card.Body>
-              {/* <Nav className="justify-content-center mb-4" variant="tabs" defaultActiveKey="/home">
-                <Nav.Item>
-                  <Nav.Link active href="/login">Login</Nav.Link>
-                </Nav.Item>
-                <Nav.Item>
-                  <Nav.Link eventKey="sign-up">Sign-Up</Nav.Link>
-                </Nav.Item> */}
-              {/* </Nav> */}
               <Card.Title className='text-center'>
                 Sign In
               </Card.Title>
@@ -43,7 +37,10 @@ function Login() {
                   Submit
                 </Button>
               </Form>
-             
+              {
+                (errorMessage === 401 || errorMessage === 404) 
+                && <div className='login-error'><InfoCircle/> Incorrect username or password</div>
+              }
             </Card.Body>
           </Card>
         </Col>
--- src/reducers/authReducer.js
+++ src/reducers/authReducer.js
@@ -3,16 +3,21 @@ import { AUTH_ERROR, AUTH_USER, GET_USER } from "../actions/types";
 const INITIAL_STATE = {
   authenticated: localStorage.getItem('token') || '',
   errorMessage: null,
-}
+  email: '',
+};
 
 const authReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
     case AUTH_USER:
-      return {...state, authenticated: action.payload.token, email: action.payload.email}    
+      return {
+        authenticated: action.payload.token, 
+        email: action.payload.email, 
+        errorMessage: null
+      };    
     case AUTH_ERROR:
-      return {...state, errorMessage: action.payload}
+      return {...state, errorMessage: action.payload};
     case GET_USER:
-      return {...state, email: action.payload.email}
+      return {...state, email: action.payload.email};
     default:
       return state;
   }
