ef2e4e8edf5c2578a9be63a792dc536b0b1fa9d0
Damian Goodenough <dgoodenough@proton.me>
2023-01-17 10:02:13 -0800

    post default lists to newly created boards

--- src/helpers/postData.js
+++ src/helpers/postData.js
@@ -3,21 +3,21 @@ import axios from 'axios'
 axios.defaults.headers.common['Authorization'] = "Bearer " + localStorage.getItem("token");
 
 export const postComment = async (cardId, commentToPost, userId) => {
-  if(commentToPost.length<1) return
-    try{
-    await axios.post(`http://localhost:5000/api/cards/${cardId}`,{
-       text: commentToPost,
-       userId: userId,
+  if (commentToPost.length < 1) return
+  try {
+    await axios.post(`http://localhost:5000/api/cards/${cardId}`, {
+      text: commentToPost,
+      userId: userId,
     })
-    .then(response => {
+      .then(response => {
         console.log(response.data);
       })
-    }
-    catch (e) {
-      console.error('Error in posting comment', e);
-      throw e;
-    }
   }
+  catch (e) {
+    console.error('Error in posting comment', e);
+    throw e;
+  }
+}
 
   export const postCard = async (listId, cardToPost, boardId) => {
     try{
@@ -25,15 +25,15 @@ export const postComment = async (cardId, commentToPost, userId) => {
       title: cardToPost,
       boardId
     })
-    .then(response => {
+      .then(response => {
         console.log(response.data);
       })
-    }
-    catch (e) {
-      console.error('Error in posting comment', e);
-      throw e;
-    }
   }
+  catch (e) {
+    console.error('Error in posting comment', e);
+    throw e;
+  }
+}
 
   export const postBoard = async (userId, newBoard, orgId) => {
     try{
@@ -41,65 +41,71 @@ export const postComment = async (cardId, commentToPost, userId) => {
           title: newBoard,
           orgId
     })
+    if (response.status === 200) {
+      ['To Do', 'Doing', 'Done'].forEach(list => {
+        axios.post(`http://localhost:5000/api/boards/${response.data.id}`, {
+          description: list,
+        })
+      })
+    }
     return response.data
-    }
-    catch(e){
-        console.error('Error in posting board', e);
-        throw e;
-    }
   }
+  catch (e) {
+    console.error('Error in posting board', e);
+    throw e;
+  }
+}
 
-  export const postList = async (boardId, newList) => {
-    try{
-       const response = await axios.post(`http://localhost:5000/api/boards/${boardId}`, {
-       description: newList
+export const postList = async (boardId, newList) => {
+  try {
+    const response = await axios.post(`http://localhost:5000/api/boards/${boardId}`, {
+      description: newList
     })
     return response.data
-    }
-    catch(e){
-        console.error('Error in posting board', e);
-        throw e;
-    }
   }
+  catch (e) {
+    console.error('Error in posting board', e);
+    throw e;
+  }
+}
 
-  export const updateBoard = async (boardId, newTitle) => {
-    try{
-       const response = await axios.put(`http://localhost:5000/api/boards/${boardId}`, {
+export const updateBoard = async (boardId, newTitle) => {
+  try {
+    const response = await axios.put(`http://localhost:5000/api/boards/${boardId}`, {
       title: newTitle
     })
     return response.data
-    }
-    catch(e){
-        console.error('Error in updating card', e);
-        throw e;
-    }
   }
+  catch (e) {
+    console.error('Error in updating card', e);
+    throw e;
+  }
+}
 
-  export const updateCard = async (cardId, newCard, newDescription) => {
-    try{
-       const response = await axios.put(`http://localhost:5000/api/cards/${cardId}`, {
+export const updateCard = async (cardId, newCard, newDescription) => {
+  try {
+    const response = await axios.put(`http://localhost:5000/api/cards/${cardId}`, {
       title: newCard
     })
     return response.data
-    }
-    catch(e){
-        console.error('Error in updating card', e);
-        throw e;
-    }
   }
+  catch (e) {
+    console.error('Error in updating card', e);
+    throw e;
+  }
+}
 
-  export const updateCardDescription = async (cardId, newDescription) => {
-    try{
-       const response = await axios.put(`http://localhost:5000/api/cards/${cardId}`, {
+export const updateCardDescription = async (cardId, newDescription) => {
+  try {
+    const response = await axios.put(`http://localhost:5000/api/cards/${cardId}`, {
       description: newDescription
     })
     return response.data
-    }
-    catch(e){
-        console.error('Error in updating card', e);
-        throw e;
-    }
   }
+  catch (e) {
+    console.error('Error in updating card', e);
+    throw e;
+  }
+}
 
-  
- 
+
