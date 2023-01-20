728ff18627cfce1e051a7385ef850548a9cd0f7a
Jack Mullinkosson <jackmullinkosson@Jacks-MacBook-Pro.local>
2023-01-20 13:51:31 -0600

    removed pencil

--- src/components/Homescreen/HomeHeader.js
+++ src/components/Homescreen/HomeHeader.js
@@ -8,7 +8,7 @@ const HomeHeader = ({title}) => {
   return (
     <div>
      <section className="home-header-title">
-     <div className='workspace-title-section'><span className='workspace-icon'><Binoculars/></span><span className='workspace-title'><h3>{title}<PencilSquare className='title-icon'/></h3></span></div>
+     <div className='workspace-title-section'><span className='workspace-icon'><Binoculars/></span><span className='workspace-title'><h3>{title}</h3></span></div>
       <button className="btn btn-info"><PersonAdd/>Invite Workspace Member</button>
      </section>
      <hr className="header-hr"></hr>
